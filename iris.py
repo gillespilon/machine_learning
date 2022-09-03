74bad1f88e97eeb6ccff7919261408cdfb4590a5
Gilles Pilon <gilles.pilon@gillespilon.ca>
2022-09-03 10:23:55 -0500

    Testing accuracy for knn=5

--- iris.py
+++ iris.py
@@ -72,6 +72,12 @@ def main():
     logreg.fit(X_train, y_train)
     y_pred = logreg.predict(X_test)
     print(metrics.balanced_accuracy_score(y_test, y_pred))
+    # train-test-split, knn with k=5
+    print("train-teset-split, testing accuracy, knn=5")
+    knn = KNeighborsClassifier(n_neighbors=5)
+    knn.fit(X_train, y_train)
+    y_pred = knn.predict(X_test)
+    print(metrics.balanced_accuracy_score(y_test, y_pred))
 
 
 if __name__ == "__main__":
