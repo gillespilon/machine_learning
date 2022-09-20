4762b41133baca3f327d9ab15ee064be28c2d1fd
Gilles Pilon <gilles.pilon@gillespilon.ca>
2022-09-20 15:06:42 -0500

    Build and evaluate a model

--- master_machine_learning_with_scikit_learn.py
+++ master_machine_learning_with_scikit_learn.py
@@ -6,28 +6,47 @@ Master machine learning with scikit-learn
 from pathlib import Path
 import time
 
+from sklearn.linear_model import LogisticRegression
+from sklearn.model_selection import cross_val_score
 import datasense as ds
 import pandas as pd
 import sklearn
 
 
 def main():
-    # Check installed versions of scikit-learn, pandas
+    print()
     print("installed scikit-learn version:", sklearn.__version__)
     print("installed pandas version:      ", pd.__version__)
-    # 2.1 Loading and exploring a dataset
-    df = pd.read_csv("titanic_train.csv", nrows=10)
+    print()
+    print("2.1 Loading and exploring a dataset")
+    print()
+    df = pd.read_csv(filepath_or_buffer="titanic_train.csv", nrows=10)
+    print("First ten rows of titanic_train.csv")
+    print()
     print(df)
     # Use intuition to select two features
     # Create the X DataFrame
     X = df[["Parch", "Fare"]]
+    print()
+    print("X DataFrame:")
     print(X)
+    print()
     # Create the y Series
     y = df["Survived"]
+    print("y Series:")
     print(y)
+    print()
     # Check the shapes of X, y
-    print(X.shape)
-    print(y.shape)
+    print("X shape:", X.shape)
+    print("y shape:", y.shape)
+    print()
+    print("2.2 Building and evaluate a model")
+    print()
+    logreg = LogisticRegression(solver="liblinear", random_state=1)
+    crossvalscore = cross_val_score(
+        estimator=logreg, X=X, y=y, cv=3, scoring="accuracy"
+    ).mean()
+    print("Cross-validation score:", crossvalscore)
 
 
 if __name__ == "__main__":
