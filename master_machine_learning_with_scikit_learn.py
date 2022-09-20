850c5d66fe7fdf71fa48655eb2d328b35c0c6d1e
Gilles Pilon <gilles.pilon@gillespilon.ca>
2022-09-20 14:28:07 -0500

    Check version scikit-learn, pandas

--- master_machine_learning_with_scikit_learn.py
+++ master_machine_learning_with_scikit_learn.py
@@ -1,6 +1,6 @@
 #! /usr/bin/env python3
 """
-docstring
+Master machine learning with scikit-learn
 """
 
 from pathlib import Path
@@ -8,10 +8,12 @@ import time
 
 import datasense as ds
 import pandas as pd
+import sklearn
 
 
 def main():
-    pass
+    print("installed scikit-learn version:", sklearn.__version__)
+    print("installed pandas version:      ", pd.__version__)
 
 
 if __name__ == "__main__":
