6ed76c821b1361d34d359a4b1ec94c4ff4439d08
Gilles Pilon <gilles.pilon@gillespilon.ca>
2022-09-20 14:43:24 -0500

    Load and explore a dataset, section 2.1

--- master_machine_learning_with_scikit_learn.py
+++ master_machine_learning_with_scikit_learn.py
@@ -12,8 +12,22 @@ import sklearn
 
 
 def main():
+    # Check installed versions of scikit-learn, pandas
     print("installed scikit-learn version:", sklearn.__version__)
     print("installed pandas version:      ", pd.__version__)
+    # 2.1 Loading and exploring a dataset
+    df = pd.read_csv("titanic_train.csv", nrows=10)
+    print(df)
+    # Use intuition to select two features
+    # Create the X DataFrame
+    X = df[["Parch", "Fare"]]
+    print(X)
+    # Create the y Series
+    y = df["Survived"]
+    print(y)
+    # Check the shapes of X, y
+    print(X.shape)
+    print(y.shape)
 
 
 if __name__ == "__main__":
