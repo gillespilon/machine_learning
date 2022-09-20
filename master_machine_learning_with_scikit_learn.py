86db179d51f7775c75bc5a723000098d8fceed1e
Gilles Pilon <gilles.pilon@gillespilon.ca>
2022-09-20 14:18:25 -0500

    Create master_machine_learning_with_scikit_learn.py

---
+++ master_machine_learning_with_scikit_learn.py
@@ -0,0 +1,18 @@
+#! /usr/bin/env python3
+"""
+docstring
+"""
+
+from pathlib import Path
+import time
+
+import datasense as ds
+import pandas as pd
+
+
+def main():
+    pass
+
+
+if __name__ == "__main__":
+    main()
