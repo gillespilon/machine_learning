d465e7661dcedac676b296c7e998f6401ac6af51
Gilles Pilon <gilles.pilon@gillespilon.ca>
2022-09-23 17:12:09 -0500

    Replace single quotes

--- example_machine_learning_basic.py
+++ example_machine_learning_basic.py
@@ -11,8 +11,8 @@ Fit a model
 Set hyperparameters for tuning
 Tune the model using grid search cross validation
 
-time -f '%e' ./lunch_and_learn_basic.py | tee lunch_and_learn_basic.txt
-time -f '%e' ./lunch_and_learn_basic.py > lunch_and_learn_basic.txt
+time -f "%e" ./lunch_and_learn_basic.py | tee lunch_and_learn_basic.txt
+time -f "%e" ./lunch_and_learn_basic.py > lunch_and_learn_basic.txt
 ./lunch_and_learn_basic.py > lunch_and_learn_basic.txt
 ./lunch_and_learn_basic.py
 """
@@ -40,24 +40,24 @@ import pandas as pd
 import numpy as np
 
 features = [
-    'X1', 'X2', 'X3', 'X4', 'X5', 'X6', 'X7',
-    'X8', 'X9', 'X10', 'X11', 'X12', 'X13', 'X14'
+    "X1", "X2", "X3", "X4", "X5", "X6", "X7",
+    "X8", "X9", "X10", "X11", "X12", "X13", "X14"
 ]
-output_url = 'lunch_and_learn_basic.html'
-header_title = 'lunch_and_learn_basic'
-graph_name = 'predicted_versus_measured'
-colour1, colour2 = '#0077bb', '#33bbee'
+output_url = "lunch_and_learn_basic.html"
+header_title = "lunch_and_learn_basic"
+graph_name = "predicted_versus_measured"
+colour1, colour2 = "#0077bb", "#33bbee"
 pd.options.display.max_columns = None
-header_id = 'lunch-and-learn-basic'
-title = 'Predicted versus Measured'
+header_id = "lunch-and-learn-basic"
+title = "Predicted versus Measured"
 pd.options.display.max_rows = None
-file_name = 'lunch_and_learn.csv'
+file_name = "lunch_and_learn.csv"
 percent_empty_features = 60.0
-set_config(display='diagram')
-label_predicted = 'Predicted'
-label_measured = 'Measured'
+set_config(display="diagram")
+label_predicted = "Predicted"
+label_measured = "Measured"
 figsize = (8, 4.5)
-target = 'Y'
+target = "Y"
 nrows = 200
 
 
@@ -65,12 +65,12 @@ def plot_scatter_y(t: pd.Series) -> NoReturn:
     y, feature = t
     fig, ax = ds.plot_scatter_y(y=y, figsize=figsize)
     ax.set_ylabel(ylabel=feature)
-    ax.set_title(label='Time Series')
+    ax.set_title(label="Time Series")
     ds.despine(ax=ax)
-    fig.savefig(fname=f'time_series_{feature}.svg', format='svg')
+    fig.savefig(fname=f"time_series_{feature}.svg", format="svg")
     ds.html_figure(
-        file_name=f'time_series_{feature}.svg',
-        caption=f'time_series_{feature}.svg'
+        file_name=f"time_series_{feature}.svg",
+        caption=f"time_series_{feature}.svg"
     )
 
 
@@ -81,7 +81,7 @@ original_stdout = ds.html_begin(
     header_id=header_id
 )
 ds.page_break()
-print('<pre style="white-space: pre-wrap;">')
+print("<pre style='white-space: pre-wrap;'>")
 # Cleaning the data
 # Data should be cleaned before fitting a model. A simple example of graphing
 # each feature in sample order and replacing outliers with NaN is shown.
@@ -103,32 +103,32 @@ with Pool() as pool:
 #         figsize=figsize
 #     )
 #     ax.set_ylabel(ylabel=feature)
-#     ax.set_title(label='Time Series')
+#     ax.set_title(label="Time Series")
 #     ds.despine(ax=ax)
 #     fig.savefig(
-#         fname=f'time_series_{feature}.svg',
-#         format='svg'
+#         fname=f"time_series_{feature}.svg",
+#         format="svg"
 #     )
 #     ds.html_figure(
-#         file_name=f'time_series_{feature}.svg',
-#         caption=f'time_series_{feature}.svg'
+#         file_name=f"time_series_{feature}.svg",
+#         caption=f"time_series_{feature}.svg"
 #     )
 
 # Set lower and upper values to remove outliers
 mask_values = [
-    ('X1', -20, 20),
-    ('X2', -25, 25),
-    ('X3', -5, 5),
-    ('X4', -10, 10),
-    ('X5', -3, 3),
-    ('X6', -5, 5),
-    ('X7', -13, 13),
-    ('X8', -9, 15),
-    ('X9', -17, 15),
-    ('X10', -16, 15),
-    ('X11', -16, 17),
-    ('X12', -16, 17),
-    ('X13', -20, 23)
+    ("X1", -20, 20),
+    ("X2", -25, 25),
+    ("X3", -5, 5),
+    ("X4", -10, 10),
+    ("X5", -3, 3),
+    ("X6", -5, 5),
+    ("X7", -13, 13),
+    ("X8", -9, 15),
+    ("X9", -17, 15),
+    ("X10", -16, 15),
+    ("X11", -16, 17),
+    ("X12", -16, 17),
+    ("X13", -20, 23)
 ]
 
 # Replace outliers with NaN
@@ -141,14 +141,17 @@ for column, lowvalue, highvalue in mask_values:
 # Delete rows if target is NaN
 data = data.dropna(subset=[target])
 
-# Remove features if number empty cells > limit
+ds.page_break()
+print("Features before threshold")
+print(features)
+print()
+# Remove features if number empty cells > threshold
 features = ds.feature_percent_empty(
     df=data,
     columns=features,
-    limit=percent_empty_features
+    threshold=percent_empty_features
 )
-ds.page_break()
-print('Features')
+print("Features after threshold")
 print(features)
 
 # Create training and testing datasets
@@ -169,7 +172,7 @@ X_train, X_test, y_train, y_test = train_test_split(
 
 # Workflow 1
 print()
-print('Workflow 1')
+print("Workflow 1")
 # Impute using the mean
 # Select features using SelectFromModel(DecisionTreeRegressor)
 # Fit with LinearRegression
@@ -181,7 +184,7 @@ imp = SimpleImputer()
 # Create the column transformer object
 ct = make_column_transformer(
     (imp, features),
-    remainder='passthrough'
+    remainder="passthrough"
 )
 
 # Create objects to use for feature selection with
@@ -207,9 +210,9 @@ xgb = XGBRegressor()
 # Create the workflow object
 pipe = Pipeline(
     steps=[
-        ('transformer', ct),
-        ('selector', selection),
-        ('regressor', linreg)
+        ("transformer", ct),
+        ("selector", selection),
+        ("regressor", linreg)
     ]
 )
 print()
@@ -220,22 +223,22 @@ pipe.fit(X_train, y_train)
 
 # Show the selected features
 print()
-print('Selected features')
+print("Selected features")
 print(X_all.columns[selection.get_support()])
 
 # Display the regression intercept
 print()
-print('Regression intercept')
+print("Regression intercept")
 print(pipe.named_steps.regressor.intercept_.round(3))
 
 # Display the regression coefficients of the features
 print()
-print('Regression coefficients')
+print("Regression coefficients")
 print(pipe.named_steps.regressor.coef_.round(3))
 
 # Cross-validate the updated pipeline
 print()
-print('Cross-validation score')
+print("Cross-validation score")
 print(cross_val_score(pipe, X_train, y_train, cv=5, n_jobs=-1).mean().round(3))
 
 # Set the hyperparameters for optimization
@@ -246,116 +249,116 @@ print(cross_val_score(pipe, X_train, y_train, cv=5, n_jobs=-1).mean().round(3))
 # 4 x 3 x 3 x 2 = 72
 hyperparams = [
     {
-        'transformer': [imp],
-        'transformer__strategy': [
-            'mean', 'median', 'most_frequent', 'constant'
+        "transformer": [imp],
+        "transformer__strategy": [
+            "mean", "median", "most_frequent", "constant"
         ],
-        'selector': [SelectFromModel(estimator=dtr_selection)],
-        'selector__threshold': [None, 'mean', 'median'],
-        'selector__estimator__criterion': [
-            'mse', 'friedman_mse', 'mae'
+        "selector": [SelectFromModel(estimator=dtr_selection)],
+        "selector__threshold": [None, "mean", "median"],
+        "selector__estimator__criterion": [
+            "mse", "friedman_mse", "mae"
         ],
-        # 'selector__estimator__splitter': ['best', 'random'],
-        # 'selector__estimator__max_features': [
-        #     None, 'auto', 'sqrt', 'log2'
+        # "selector__estimator__splitter": ["best", "random"],
+        # "selector__estimator__max_features": [
+        #     None, "auto", "sqrt", "log2"
         # ],
-        # 'selector__estimator__max_leaf_nodes': [None, 2, 4, 6],
-        'regressor': [linreg],
-        'regressor__normalize': [False, True]
+        # "selector__estimator__max_leaf_nodes": [None, 2, 4, 6],
+        "regressor": [linreg],
+        "regressor__normalize": [False, True]
     },
 
 ]
-'''hyperparams.append(
+"""hyperparams.append(
     {
-        'transformer': [imp],
-        'transformer__strategy': [
-            'mean', 'median', 'most_frequent', 'constant'
+        "transformer": [imp],
+        "transformer__strategy": [
+            "mean", "median", "most_frequent", "constant"
         ],
-        'selector': [SelectFromModel(estimator=linreg_selection)],
-        'selector__threshold': [None, 'mean', 'median'],
-        'selector__estimator__normalize': [False, True],
-        'regressor': [linreg],
-        'regressor__normalize': [False, True]
+        "selector": [SelectFromModel(estimator=linreg_selection)],
+        "selector__threshold": [None, "mean", "median"],
+        "selector__estimator__normalize": [False, True],
+        "regressor": [linreg],
+        "regressor__normalize": [False, True]
     },
-)'''
-'''
+)"""
+"""
 hyperparams.append(
    {
-        'transformer': [imp],
-        'transformer__strategy': [
-            'mean', 'median', 'most_frequent', 'constant'
+        "transformer": [imp],
+        "transformer__strategy": [
+            "mean", "median", "most_frequent", "constant"
         ],
-        'selector': [SelectFromModel(estimator=lasso_selection)],
-        'selector__threshold': [None, 'mean', 'median'],
-        'selector__estimator__normalize': [False, True],
-        'regressor': [linreg],
-        'regressor__normalize': [False, True]
+        "selector": [SelectFromModel(estimator=lasso_selection)],
+        "selector__threshold": [None, "mean", "median"],
+        "selector__estimator__normalize": [False, True],
+        "regressor": [linreg],
+        "regressor__normalize": [False, True]
     },
 )
-'''
+"""
 # 4 x 3 x 2 x 2 = 48
 hyperparams.append(
    {
-        'transformer': [imp],
-        'transformer__strategy': [
-            'mean', 'median', 'most_frequent', 'constant'
+        "transformer": [imp],
+        "transformer__strategy": [
+            "mean", "median", "most_frequent", "constant"
         ],
-        'selector': [SelectFromModel(estimator=lassocv_selection)],
-        'selector__threshold': [None, 'mean', 'median'],
-        'selector__estimator__normalize': [False, True],
-        'regressor': [linreg],
-        'regressor__normalize': [False, True]
+        "selector": [SelectFromModel(estimator=lassocv_selection)],
+        "selector__threshold": [None, "mean", "median"],
+        "selector__estimator__normalize": [False, True],
+        "regressor": [linreg],
+        "regressor__normalize": [False, True]
     },
 )
-'''
+"""
 hyperparams.append(
 
     {
 
-        'transformer': [imp],
+        "transformer": [imp],
 
-        'transformer__strategy': [
+        "transformer__strategy": [
 
-            'mean', 'median', 'most_frequent', 'constant'
+            "mean", "median", "most_frequent", "constant"
 
         ],
 
-        'selector': [SelectFromModel(estimator=rfr_selection)],
+        "selector": [SelectFromModel(estimator=rfr_selection)],
 
-        'selector__threshold': [None, 'mean', 'median'],
+        "selector__threshold": [None, "mean", "median"],
 
-        'selector__estimator__criterion': ['mse', 'mae'],
+        "selector__estimator__criterion": ["mse", "mae"],
 
-        'regressor': [linreg],
+        "regressor": [linreg],
 
-        'regressor__normalize': [False, True]
+        "regressor__normalize": [False, True]
 
     },
 
 )
-'''
+"""
 
 # Perform a grid search
 grid = GridSearchCV(pipe, hyperparams, n_jobs=-1, cv=5)
 grid.fit(X_train, y_train)
 
 # Present the results
-pd.DataFrame(grid.cv_results_).sort_values('rank_test_score')
+pd.DataFrame(grid.cv_results_).sort_values("rank_test_score")
 
 # Access the best score
 print()
-print('Hyperparameter optimization')
-print('Best score')
+print("Hyperparameter optimization")
+print("Best score")
 print(grid.best_score_.round(3))
 
 # Access the best hyperparameters
 print()
-print('Best hyperparameters')
+print("Best hyperparameters")
 print(grid.best_params_)
 
 # Workflow 2
 ds.page_break()
-print('Workflow 2')
+print("Workflow 2")
 # Impute using the mean
 # Select features using SelectFromModel(LassoCV())
 # Fit with LinearRegression()
@@ -364,14 +367,14 @@ imp = SimpleImputer()
 # Create the column transformer object
 ct = make_column_transformer(
      (imp, features),
-     remainder='passthrough'
+     remainder="passthrough"
 )
 # Create the object to use for feature selection
 lassocv_selection = LassoCV()
 # Create the feature selection object
 selection = SelectFromModel(
     estimator=lassocv_selection,
-    threshold='median'
+    threshold="median"
 )
 # Create objects to use for regression
 linreg = LinearRegression()
@@ -385,7 +388,7 @@ pipe.fit(X_train, y_train)
 # selected = pd.DataFrame(X.columns[selection.get_support()])
 selected_features = X_all.columns[selection.get_support()].to_list()
 print()
-print('Selected features')
+print("Selected features")
 print(selected_features)
 selected_coefficients = pipe.named_steps.linearregression.coef_.round(3)
 selected_importances = np.abs(
@@ -395,26 +398,26 @@ print()
 print(pd.DataFrame(
     list(zip(
         selected_features, selected_importances, selected_coefficients)),
-    columns=['Features', 'Importance', 'Coefficients']
+    columns=["Features", "Importance", "Coefficients"]
 ))
 # Display the regression intercept
 print()
-print('Regression intercept')
+print("Regression intercept")
 print(pipe.named_steps.linearregression.intercept_.round(3))
 print()
-print('Cross-validation score')
+print("Cross-validation score")
 print(cross_val_score(
     pipe, X_train, y_train, cv=5, n_jobs=-1,
-    scoring='r2'
+    scoring="r2"
 ).mean().round(3))
 # Calculate predicted values
 predicted = cross_val_predict(pipe, X_all, y_all, cv=5, n_jobs=-1)
 mse = mean_squared_error(y_all, predicted)
 print()
-print('Mean squared error')
+print("Mean squared error")
 print(mse.round(3))
 print()
-print('Root mean squared error')
+print("Root mean squared error")
 print(round(math.sqrt(mse), 3))
 ds.page_break()
 # Scatter plot of predicted versus measured
@@ -427,7 +430,7 @@ ax.plot(
     [y_all.min(), y_all.max()],
     [y_all.min(), y_all.max()],
     marker=None,
-    linestyle='-',
+    linestyle="-",
     color=colour2
 )
 ax.set_ylabel(ylabel=label_predicted)
@@ -435,12 +438,12 @@ ax.set_xlabel(xlabel=label_measured)
 ax.set_title(label=title)
 ds.despine(ax=ax)
 fig.savefig(
-    fname=f'{graph_name}_scatter.svg',
-    format='svg'
+    fname=f"{graph_name}_scatter.svg",
+    format="svg"
 )
 ds.html_figure(
-    file_name=f'{graph_name}_scatter.svg',
-    caption=f'{graph_name}_scatter.svg'
+    file_name=f"{graph_name}_scatter.svg",
+    caption=f"{graph_name}_scatter.svg"
 )
 # Line plot of predicted versus measured
 fig, ax = ds.plot_line_line_y1_y2(
@@ -454,12 +457,12 @@ ax.legend(frameon=False)
 ax.set_title(label=title)
 ds.despine(ax=ax)
 fig.savefig(
-    fname=f'{graph_name}_lines.svg',
-    format='svg'
+    fname=f"{graph_name}_lines.svg",
+    format="svg"
 )
 ds.html_figure(
-    file_name=f'{graph_name}_lines.svg',
-    caption=f'{graph_name}_lines.svg'
+    file_name=f"{graph_name}_lines.svg",
+    caption=f"{graph_name}_lines.svg"
 )
 stop_time = time.time()
 ds.page_break()
@@ -467,7 +470,7 @@ ds.report_summary(
     start_time=start_time,
     stop_time=stop_time
 )
-print('</pre>')
+print("</pre>")
 ds.html_end(
     original_stdout=original_stdout,
     output_url=output_url
