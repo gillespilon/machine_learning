6e1aa5859897c181b37d36aa1c9a27c12bc5e69a
Gilles Pilon <gilles.pilon@gillespilon.ca>
2022-09-29 13:27:18 -0500

    Edit for all data rows to be included

--- titanic_predict.py
+++ titanic_predict.py
@@ -11,22 +11,18 @@ import joblib
 
 
 def main():
-    features = ["Parch", "Fare", "Embarked", "Sex", "Name"]
+    features = ["Parch", "Fare", "Embarked", "Sex", "Name", "Age"]
     titanic_joblib = Path("pipeline.joblib")
     file_test = "titanic_new.csv"
     pipeline_from_joblib = joblib.load(filename=titanic_joblib)
     # df_test = pd.read_csv(
-    #     filepath_or_buffer=file_test,
-    #     nrows=10
+    #     filepath_or_buffer=file_test
     # )
     df_test = ds.read_file(
-        file_name=file_test,
-        nrows=10
+        file_name=file_test
     )
     X_test = df_test[features]
     predictions_ndarray = pipeline_from_joblib.predict(X=X_test)
-    print("Predictions X_test:", predictions_ndarray)
-    print()
     predictions_series = pd.Series(
         data=predictions_ndarray,
         index=X_test.index,
@@ -36,10 +32,6 @@ def main():
         objs=[X_test, predictions_series],
         axis='columns'
     )
-    print("Create X_test with predictions:")
-    print()
-    print(X_test_predictions)
-    print()
 
 
 if __name__ == "__main__":
