d0378e342220f7a186cfe463968b2a92ed832e57
Gilles Pilon <gilles.pilon@gillespilon.ca>
2022-10-04 16:33:30 -0500

    Update docstring

--- outliers_masking_pipeline_scikit_learn.py
+++ outliers_masking_pipeline_scikit_learn.py
@@ -1,6 +1,12 @@
 #! /usr/bin/env python3
 """
 Code to mask outliers with a function, column transformer, and pipeline
+
+The goal is to mask outliers and then process these within scikit-learn,
+not within pandas, in order to avoid data leakage.
+
+./outliers_masking_pipeline_scikit_learn.py >
+    outliers_masking_pipeline_scikit_learn.txt
 """
 
 from pathlib import Path
@@ -16,6 +22,7 @@ def main():
     target = "Y"
     print("outliers_masking_pipeline_scikit_learn.py")
     print()
+    # create a dataset
     df = ds.read_file(file_name=file_name)
     ds.dataframe_info(
         df=df,
