219b3df53a5e19127565aad256d4bf71f0236709
Gilles Pilon <gilles.pilon@gillespilon.ca>
2022-10-13 09:17:15 -0500

    Clean data with scikit-learn

--- machine_learning_barebones.py
+++ machine_learning_barebones.py
@@ -1,80 +1,230 @@
 #! /usr/bin/env python3
 """
-Example of scikit-learn for supervised machine learning, Xs are measurements
+Example of scikit-learn for supervised machine learning
+
+- Xs are measurements
+- Ys are measurements
+- Process missing X values within scikit-learn
+- Tune hyperparameters
 """
 
+from typing import List, Tuple
 from pathlib import Path
-import time
 
+from sklearn.linear_model import Lasso, LassoCV, LinearRegression
+from sklearn.model_selection import cross_val_score, GridSearchCV
 from sklearn.feature_selection import SelectFromModel
+from sklearn.preprocessing import FunctionTransformer
 from sklearn.pipeline import make_pipeline, Pipeline
 from sklearn.compose import make_column_transformer
-from sklearn.linear_model import LinearRegression
+from sklearn.ensemble import RandomForestRegressor
+from sklearn.tree import DecisionTreeRegressor
 from sklearn.impute import SimpleImputer
-import datasense as ds
+from xgboost import XGBRegressor
 import pandas as pd
+import numpy as np
 
 
-def main():
-    features = [
-        "X1", "X2", "X3", "X4", "X5", "X6", "X7",
-        "X8", "X9", "X10", "X11", "X12", "X13", "X14"
-    ]
-    file_name = "lunch_and_learn.csv"
-    target = "Y"
-    print("example_machine_learning_barebones.py")
-    print()
-    df = ds.read_file(file_name=file_name)
-    # scikit-learn does not support removing outlines within a Pipeline
-# Set lower and upper values to remove outliers
-    mask_values = [
-        ("X1", -20, 20),
-        ("X2", -25, 25),
-        ("X3", -5, 5),
-        ("X4", -10, 10),
-        ("X5", -3, 3),
-        ("X6", -5, 5),
-        ("X7", -13, 13),
-        ("X8", -9, 15),
-        ("X9", -17, 15),
-        ("X10", -16, 15),
-        ("X11", -16, 17),
-        ("X12", -16, 17),
-        ("X13", -20, 23)
-    ]
-    ds.dataframe_info(
-        df=df,
-        file_in=file_name
-    )
-    # Replace outliers with NaN
-    for column, lowvalue, highvalue in mask_values:
+def mask_outliers(
+    df: pd.DataFrame,
+    maskvalues: List[Tuple[str, float, float]]
+) -> pd.DataFrame:
+    """
+    Mask outliers.
+
+    Parameters
+    ----------
+    df : pd.DataFrame
+        The input DataFrame.
+
+    Returns
+    -------
+    df : pd.DataFrame
+        The output DataFrame.
+    """
+    for column, lowvalue, highvalue in maskvalues:
         df[column] = df[column].mask(
             cond=(df[column] <= lowvalue) | (df[column] >= highvalue),
             other=pd.NA
         )
-    ds.dataframe_info(
-        df=df,
-        file_in=file_name
+    return pd.DataFrame(data=df)
+
+
+def main():
+    file_predictions = Path("outliers_missing_predictions.csv")
+    features = [
+        "X1", "X2", "X3", "X4", "X5", "X6", "X7",
+        "X8", "X9", "X10", "X11", "X12", "X13", "X14"
+    ]
+    maskvalues = [
+        ("X1", -10, 10),
+        ("X2", -25, 25),
+        ("X3", -5, 5),
+        ("X4", -7, 7),
+        ("X5", -3, 3),
+        ("X6", -2, 2),
+        ("X7", -13, 13),
+        ("X8", -8, 8),
+        ("X9", -9, 9),
+        ("X10", -10, 10),
+        ("X11", -9, 9),
+        ("X12", -16, 17),
+        ("X13", -20, 23)
+    ]
+    file_new = Path("outliers_missing_new.csv")
+    file_data = Path("outliers_missing.csv")
+    pd.options.display.max_columns = None
+    pd.options.display.max_rows = None
+    target = "Y"
+    nrows = 200
+    df = pd.read_csv(
+        filepath_or_buffer=file_data,
+        skip_blank_lines=False,
+        nrows=nrows
     )
-    # delete empty rows for the target or impute missing values
     df = df.dropna(subset=[target])
-    # df[target] = df[target].fillna(df[target].mean())
     X = df[features]
     y = df[target]
-    # impute mean for missing target values or delete empty rows
-    simple_imputer = SimpleImputer(missing_values=pd.NA)
-    column_transformer = make_column_transformer(
-        (simple_imputer, features),
-        remainder="passthrough"
+    df_new = pd.read_csv(
+        filepath_or_buffer=file_new,
+        skip_blank_lines=False
     )
-    linear_regression= LinearRegression()
-    pipeline = make_pipeline(column_transformer, linear_regression)
-    pipeline.fit(X=X, y=y)
+    X_new = df_new[features]
+    print("Workflow 1")
+    print()
+    mask = FunctionTransformer(
+        mask_outliers,
+        kw_args={"maskvalues": maskvalues}
+    )
+    imputer = SimpleImputer()
+    imputer_pipeline = make_pipeline(mask, imputer)
+    transformer = make_column_transformer(
+        (imputer_pipeline, features),
+        remainder="drop"
+    )
+    decision_tree_regressor = DecisionTreeRegressor()
+    random_forest_regressor = RandomForestRegressor()
+    linear_regression = LinearRegression()
+    xgboost_regressor = XGBRegressor()
+    lassocv = LassoCV()
+    lasso = Lasso()
+    selector = SelectFromModel(estimator=decision_tree_regressor)
+    hyperparameters1 = {}
+    hyperparameters1["transformer"] = [imputer]
+    hyperparameters1["transformer__strategy"] = [
+        "mean", "median", "most_frequent", "constant"
+    ]
+    hyperparameters1["selector"] = [
+        SelectFromModel(estimator=decision_tree_regressor)
+    ]
+    hyperparameters1["selector__threshold"] = [None, "mean", "median"]
+    hyperparameters1["selector__estimator__criterion"] = [
+        "squared_error", "friedman_mse", "absolute_error"
+    ]
+    hyperparameters1["selector__estimator__splitter"] = ["best", "random"]
+    hyperparameters1["selector__estimator__max_features"] = [
+        None, "sqrt", "log2"
+    ]
+    hyperparameters1["regressor"] = [linear_regression]
+    hyperparameters2 = {}
+    hyperparameters2["transformer"] = [imputer]
+    hyperparameters2["transformer__strategy"] = [
+        "mean", "median", "most_frequent", "constant"
+    ]
+    hyperparameters2["selector"] = [SelectFromModel(estimator=lassocv)]
+    hyperparameters2["selector__threshold"] = [None, "mean", "median"]
+    hyperparameters2["regressor"] = [linear_regression]
+    hyperparameters = [hyperparameters1, hyperparameters2]
+    pipeline = Pipeline(
+        steps=[
+            ("transformer", transformer),
+            ("selector", selector),
+            ("regressor", linear_regression)
+        ]
+    )
+    print(pipeline)
+    pipeline.fit(
+        X=X,
+        y=y
+    )
+    print()
+    print("Hyperparamters:")
+    print(hyperparameters)
+    print()
+    grid_search = GridSearchCV(
+        estimator=pipeline,
+        param_grid=hyperparameters,
+        scoring="r2",
+        n_jobs=-1,
+        cv=10
+    )
+    grid_search.fit(
+        X=X,
+        y=y
+    )
+    print("Best hyperparameters")
+    print(grid_search.best_params_)
+    print()
+    print("Best score")
+    print(grid_search.best_score_.round(3))
+    print()
+    print("Workflow 2")
+    print()
+    imputer = SimpleImputer(strategy="mean")
+    transformer = make_column_transformer(
+         (imputer, features),
+         remainder="passthrough"
+    )
+    selector = SelectFromModel(
+        estimator=lassocv,
+        threshold="median"
+    )
+    pipeline = make_pipeline(transformer, selector, linear_regression)
+    print(pipeline)
+    print()
+    pipeline.fit(
+        X=X,
+        y=y
+    )
+    selected_features = X.columns[selector.get_support()].to_list()
+    print("Selected features")
+    selected_coefficients = pipeline.named_steps.linearregression.\
+        coef_.round(3)
+    selected_importances = np.abs(
+        pipeline.named_steps.selectfrommodel.estimator_.coef_[
+            selector.get_support()
+        ]
+    ).tolist()
+    print(pd.DataFrame(
+        list(zip(
+            selected_features, selected_importances, selected_coefficients)),
+        columns=["Features", "Importance", "Coefficients"]
+    ))
+    print()
     print("Regression intercept")
     print(pipeline.named_steps.linearregression.intercept_.round(3))
-    print("Regression coefficients")
-    print(pipeline.named_steps.linearregression.coef_.round(3))
     print()
+    print("Cross-validation score")
+    print(cross_val_score(
+        estimator=pipeline,
+        X=X,
+        y=y,
+        scoring="r2",
+        cv=10,
+        n_jobs=-1
+    ).mean().round(3))
+    print()
+    predictions_ndarray = grid_search.predict(X=X_new)
+    predictions_series = pd.Series(
+        data=predictions_ndarray,
+        index=X_new.index,
+        name="y predictions"
+    )
+    X_new_predictions = pd.concat(
+        objs=[X_new, predictions_series],
+        axis="columns"
+    )
+    X_new_predictions.to_csv(path_or_buf=file_predictions)
 
 
 if __name__ == "__main__":
