0cf783258fe4f44983e9bfe1d6852ad44981db20
Gilles Pilon <gilles.pilon@gillespilon.ca>
2023-01-04 08:49:35 -0600

    Add html output, read entire cvs files

--- master_machine_learning_with_scikit_learn.py
+++ master_machine_learning_with_scikit_learn.py
@@ -3,6 +3,8 @@
 Essential code for Kevin Markham's "Master machine learning with scikit-learn."
 """
 
+from pathlib import Path
+
 from sklearn.feature_extraction.text import CountVectorizer
 from sklearn.linear_model import LogisticRegression
 from sklearn.compose import make_column_transformer
@@ -15,23 +17,41 @@ import datasense as ds
 def main():
     features = ["Parch", "Fare", "Embarked", "Sex", "Name", "Age"]
     one_hot_encoder_features = ["Embarked", "Sex"]
+    output_url = "master_machine_learning.html"
+    header_title = "Master Machine Learning"
     passthrough_features = ["Parch", "Fare"]
+    header_id = "master-machine-learning"
     file_data = "titanic_data.csv"
     file_new = "titanic_new.csv"
     vectorizer_features = "Name"
     imputer_features = ["Age"]
     target = "Survived"
-    print("master_machine_learning_with_scikit_learn.py")
-    print()
+    original_stdout = ds.html_begin(
+        output_url=output_url,
+        header_title=header_title,
+        header_id=header_id
+    )
+    ds.script_summary(
+        script_path=Path(__file__),
+        action="started at"
+    )
     df = ds.read_file(
         file_name=file_data,
-        nrows=10
+        # nrows=10
+    )
+    ds.dataframe_info(
+        df=df,
+        file_in=file_data
     )
     X = df[features]
     y = df[target]
     df_new = ds.read_file(
         file_name=file_new,
-        nrows=10
+        # nrows=10
+    )
+    ds.dataframe_info(
+        df=df_new,
+        file_in=file_new
     )
     X_new = df_new[features]
     one_hot_encoder = OneHotEncoder()
@@ -58,6 +78,8 @@ def main():
     pipeline.predict(X=X_new)
     print("pipeline.predict(X=X_new):", pipeline.predict(X=X_new))
     print()
+    ds.script_summary(script_path=Path(__file__), action="finished at")
+    ds.html_end(original_stdout=original_stdout, output_url=output_url)
 
 
 if __name__ == "__main__":
