c9d9afc3d85ebc4567b55162c0fd7946520a2719
Gilles Pilon <gilles.pilon@gillespilon.ca>
2023-01-20 12:02:42 -0600

    Capitalize constant variables

--- master_machine_learning_with_scikit_learn.py
+++ master_machine_learning_with_scikit_learn.py
@@ -10,6 +10,7 @@ from sklearn.feature_extraction.text import CountVectorizer
 from sklearn.linear_model import LogisticRegression
 from sklearn.compose import make_column_transformer
 from sklearn.preprocessing import OneHotEncoder
+from pandas.api.types import CategoricalDtype
 from sklearn.pipeline import make_pipeline
 from sklearn.impute import SimpleImputer
 import datasense as ds
@@ -17,45 +18,49 @@ import datasense as ds
 
 def main():
     start_time = time.perf_counter()
-    features = ["Parch", "Fare", "Embarked", "Sex", "Name", "Age"]
-    one_hot_encoder_features = ["Embarked", "Sex"]
-    output_url = "master_machine_learning.html"
-    header_title = "Master Machine Learning"
-    header_id = "master-machine-learning"
-    imputer_features = ["Age", "Fare"]
-    passthrough_features = ["Parch"]
-    file_data = "titanic_data.csv"
-    file_new = "titanic_new.csv"
-    vectorizer_features = "Name"
-    target = "Survived"
+    FEATURES = ["Parch", "Fare", "Embarked", "Sex", "Name", "Age"]
+    ONE_HOT_ENCODER_FEATURES = ["Embarked", "Sex"]
+    OUTPUT_URL = "master_machine_learning.html"
+    HEADER_TITLE = "Master Machine Learning"
+    HEADER_ID = "master-machine-learning"
+    IMPUTER_FEATURES = ["Age", "Fare"]
+    PASSTHROUGH_FEATURES = ["Parch"]
+    FILE_DATA = "titanic_data.csv"
+    FILE_NEW = "titanic_new.csv"
+    VECTORIZER_FEATURES = "Name"
+    TARGET = "Survived"
     original_stdout = ds.html_begin(
-        output_url=output_url,
-        header_title=header_title,
-        header_id=header_id
+        output_url=OUTPUT_URL,
+        header_title=HEADER_TITLE,
+        header_id=HEADER_ID
     )
     ds.script_summary(
         script_path=Path(__file__),
         action="started at"
     )
+    # Read training data, define X and y
     df = ds.read_file(
-        file_name=file_data,
+        file_name=FILE_DATA,
         # nrows=10
-    )
+    ).astype(dtype={"Embarked": CategoricalDtype()})
+    print(df.info())
     ds.dataframe_info(
         df=df,
-        file_in=file_data
+        file_in=FILE_DATA
     )
-    X = df[features]
-    y = df[target]
+    X = df[FEATURES]
+    y = df[TARGET]
+    # Read new data, define X_new
     df_new = ds.read_file(
-        file_name=file_new,
+        file_name=FILE_NEW,
         # nrows=10
-    )
+    ).astype(dtype={"Embarked": CategoricalDtype()})
+    print(df.info())
     ds.dataframe_info(
         df=df_new,
-        file_in=file_new
+        file_in=FILE_NEW
     )
-    X_new = df_new[features]
+    X_new = df_new[FEATURES]
     print("Missing data")
     print("============")
     print(
@@ -67,24 +72,30 @@ def main():
         "Cabin is not a Feature in our model."
     )
     print()
-    one_hot_encoder = OneHotEncoder()
-    vectorizer = CountVectorizer()
+    # Create four instances of transformers
     imputer = SimpleImputer()
     imputer_constant = SimpleImputer(
         strategy="constant",
         fill_value="missing"
     )
+    one_hot_encoder = OneHotEncoder()
+    vectorizer = CountVectorizer()
+    # Create two-step transformer pipeline of constant value imputation and
+    # one-hot encoding
     pipeline_imputer_ohe = make_pipeline(imputer_constant, one_hot_encoder)
+    # Create column transformer
     column_transformer = make_column_transformer(
-        (pipeline_imputer_ohe, one_hot_encoder_features),
-        (vectorizer, vectorizer_features),
-        (imputer, imputer_features),
-        ("passthrough", passthrough_features)
+        (pipeline_imputer_ohe, ONE_HOT_ENCODER_FEATURES),
+        (vectorizer, VECTORIZER_FEATURES),
+        (imputer, IMPUTER_FEATURES),
+        ("passthrough", PASSTHROUGH_FEATURES)
     )
+    # Create instance of logistic regression
     logistic_regression = LogisticRegression(
         solver="liblinear",
         random_state=1
     )
+    # Create two-step pipeline, fit the pipeline, make predictions
     pipeline = make_pipeline(column_transformer, logistic_regression)
     pipeline.fit(X=X, y=y)
     pipeline.predict(X=X_new)
@@ -102,7 +113,7 @@ def main():
     stop_time = time.perf_counter()
     ds.script_summary(script_path=Path(__file__), action="finished at")
     ds.report_summary(start_time=start_time, stop_time=stop_time)
-    ds.html_end(original_stdout=original_stdout, output_url=output_url)
+    ds.html_end(original_stdout=original_stdout, output_url=OUTPUT_URL)
 
 
 if __name__ == "__main__":
